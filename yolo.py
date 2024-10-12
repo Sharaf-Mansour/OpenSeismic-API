1fc60f33f88312063d8818f58c8009978ef3a82b
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2024-10-12 20:57:33 +0300

    Create yolo.py

---
+++ yolo.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+import torch
+def predict(file_name):
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = YOLO(r"best.pt").to(device)
+    model.predict(source = file_name , save = True , conf=0.5 , project = 'lunarannotations' , name='annotation' , iou =0.5) 
+    
